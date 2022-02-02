b9e9c9f60fd0d01e83523b228e0910241aea2fab
munozlucas <lgaston.munoz@gmail.com>
2022-02-02 10:42:56 -0300

    add import modules

---
+++ src/components/project-input.ts
@@ -0,0 +1,78 @@
+import { Component } from "./base-component.js"
+import { autobind } from "../decorators/autobind.js"
+import { Validatable, validate } from '../util/validation.js';
+import { projectState } from "../state/project-state.js";
+
+export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+  titleInputElement: HTMLInputElement
+  descriptionInputElement: HTMLInputElement
+  peopleInputElement: HTMLInputElement
+
+  constructor() {
+    super('project-input', 'app', true, 'user-input')
+
+    this.titleInputElement = this.element.querySelector('#title')!
+    this.descriptionInputElement = this.element.querySelector('#description')!
+    this.peopleInputElement = this.element.querySelector('#people')!
+
+    this.configure()
+  }
+
+  renderContent(): void {}
+
+  configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this))
+  }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault()
+    const userInput = this.gatherUserInput()
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput
+      projectState.addProject(title, description, people)
+      this.clearInput()
+    }
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value
+    const enteredDescription = this.descriptionInputElement.value
+    const enteredPeople = this.peopleInputElement.value
+
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true
+    }
+
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5
+    }
+
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5
+    }
+
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert('invalid input')
+      return
+    }
+
+    return [enteredTitle, enteredDescription, +enteredPeople]
+  }
+
+  private clearInput() {
+    this.titleInputElement.value = ''
+    this.descriptionInputElement.value = ''
+    this.peopleInputElement.value = ''
+  }
+}
